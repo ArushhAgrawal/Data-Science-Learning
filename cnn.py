5462bafa15bf7cc6c9e80c468e9b833c2d5be1da
Arush Agrawal <arushagrawal366@gmail.com>
2026-06-25 17:16:58 +0530

    decreased train test time by 50%  and increased accuracy

--- cnn.py
+++ cnn.py
@@ -20,7 +20,7 @@ train_dataloader= DataLoader(dataset=train_data,
                             shuffle=True)
 test_dataloader= DataLoader(dataset=test_data,
                             batch_size=32,
-                            shuffle=True)
+                            shuffle=False)
 images, label= next(iter(train_dataloader))
 # print(images.shape)
 
@@ -35,19 +35,19 @@ class FashionModel(nn.Module):
                                         nn.Conv2d(in_channels=10, out_channels=16, kernel_size=3,padding=1,stride=1),
                                         nn.ReLU(),
                                         nn.MaxPool2d(kernel_size=2, stride=2 )#stride is by default set to match the kernel value
-                                        )      
+                                        ).to(device)    
         self.conv_block2=nn.Sequential(nn.Conv2d(in_channels=16, out_channels=10, kernel_size=3,padding=1,stride=1),
                                        nn.ReLU(),
                                        nn.Conv2d(in_channels=10, out_channels=16, kernel_size=3,padding=1,stride=1),
-                                       nn.MaxPool2d(kernel_size=2, stride=2))         
+                                       nn.MaxPool2d(kernel_size=2, stride=2)).to(device)      
         self.classifier= nn.Sequential(nn.Flatten(),
-                                       nn.Linear(in_features=16*7*7, out_features=10))
+                                       nn.Linear(in_features=16*7*7, out_features=10)
+                                       ).to(device)
     def forward(self, x):
         return self.classifier(self.conv_block2(self.conv_block1(x)))
 
 #instantiating model
 model= FashionModel()
-
 #defing loss and optimizer functions
 loss_fn= nn.CrossEntropyLoss()
 optimizer= torch.optim.Adam(model.parameters(), lr=0.001)
@@ -60,22 +60,35 @@ optimizer= torch.optim.Adam(model.parameters(), lr=0.001)
 #         loss=loss_fn(y1,y)
 # print(y1.shape)
 
+#defining accuracy
+def accuracy(y_pred, y_true):
+    correct= torch.eq(y_pred,y_true.to(device)).sum()
+    acc= correct/len(y_true.to(device))*100
+    return acc
 #training loop
 torch.manual_seed(32)
-epochs=1
+torch.mps.manual_seed(32)
+epochs=3
 for epoch in range(epochs):
+    acc_test=0
+    acc_train=0
     model.train()
     for batch, (x,y) in enumerate(train_dataloader):
-        y_logits_train=model(x)
+        y_logits_train=model(x.to(device))
         y_pred_train= torch.argmax(y_logits_train, dim=1)
-        loss_train= loss_fn(y_logits_train, y)
+        loss_train= loss_fn(y_logits_train, y.to(device))
         optimizer.zero_grad()
         loss_train.backward()
         optimizer.step()
+        acc_train+= accuracy(y_pred= y_pred_train, y_true=y)
+    avg_acc_train=acc_train/len(train_dataloader)
     model.eval()
     for batch, (x,y) in enumerate(test_dataloader):
         with torch.inference_mode():
-            y_logits_test= model(x)
+            y_logits_test= model(x.to(device))
             y_pred_test= torch.argmax(y_logits_test, dim=1)
-            loss_test= loss_fn(y_logits_test, y)
-print(loss_test, loss_train)
+            loss_test= loss_fn(y_logits_test, y.to(device))
+            acc_test+= accuracy(y_pred= y_pred_test, y_true=y)
+    avg_acc_test=acc_test/len(test_dataloader)
+print(f"train loss: {loss_test} \nand test  {loss_train}")
+print(f"Average accuracy of train: {avg_acc_train} \nand test {avg_acc_test}")
