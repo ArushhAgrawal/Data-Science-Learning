283ece7074950568cb32292f7445560d983d45bd
Arush Agrawal <arushagrawal366@gmail.com>
2026-07-10 16:40:48 +0530

    making model using numpy as core,  loaded dataset and converted it to numpy

---
+++ np_model.py
@@ -0,0 +1,30 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets,transforms
+import numpy as np
+from torchvision.transforms import ToTensor
+#loadining data
+train_data=datasets.MNIST(root= "number_data",
+                          train=True,
+                          transform=ToTensor,
+                          target_transform=None,
+                          download=False)
+test_data= datasets.MNIST(root= "number_data",
+                          train= False,
+                          transform=transforms.ToTensor,
+                          target_transform=None,
+                          download=False)
+
+#conversion to np
+x_train= train_data.data.numpy()
+x_train=x_train[:10000]
+x_test= test_data.data.numpy()
+x_test=x_test[:10000]
+#for labels
+y_labels_train= train_data.targets.numpy()
+y_labels_train= y_labels_train[:10000]
+y_labels_test= test_data.targets.numpy()
+y_labels_test= y_labels_test[:10000]
+
+print(y_labels_test)
+
