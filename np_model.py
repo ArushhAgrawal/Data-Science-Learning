c9a3acacc503e46eacb3dd5ff6aef8cea349f3da
Arush Agrawal <arushagrawal366@gmail.com>
2026-07-11 11:55:48 +0530

    defined the nuron, need little fixes and add ons

--- np_model.py
+++ np_model.py
@@ -18,18 +18,29 @@ test_data= datasets.MNIST(root= "number_data",
 #conversion to np
 x_train= train_data.data.numpy()
 x_train=x_train[:10000]
+x_train= x_train.reshape(10000,784)
 x_test= test_data.data.numpy()
-x_test=x_test[:10000]
+x_test=x_test[:1000]
+x_test= x_test.reshape(1000,784)
 #for labels
 y_labels_train= train_data.targets.numpy()
 y_labels_train= y_labels_train[:10000]
 y_labels_test= test_data.targets.numpy()
-y_labels_test= y_labels_test[:10000]
-print(y_labels_test.shape)
+y_labels_test= y_labels_test[:1000]
+# print(y_labels_test.shape)
 
-# def loss(y_pred, y_true):
-#     i=1
-#     for i in range(10):
-#         y_pred[i-1:i]#finding the predicited output
-        
+def loss(y_pred, y_true):
+    i=0
+    cost= np.array([])
+    for i in range(10):
+        if i== y_true:
+            cost= np.append(cost, (y_pred[y_true]- 1)**2)
+        else:
+            cost= np.append(cost, (y_pred[i]-0)**2)
+    return cost.mean()
 
+weight= np.random.random((784,10))
+print(weight.shape)
+
+bias= np.random.random((10))
+print(bias)
