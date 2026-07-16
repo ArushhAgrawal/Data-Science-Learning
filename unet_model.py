1c281f26b71686c4530dae260735491cc04bcb9b
Arush Agrawal <arushagrawal366@gmail.com>
2026-07-16 20:29:06 +0530

    tested and completed the model brain

--- unet_model.py
+++ unet_model.py
@@ -18,17 +18,44 @@ class DoubleConv(nn.Module):
         return self.convstack(x)
 
 class UNET(nn.Module):
-    def __init__(self, in_channels=3,  out_features=1, features=[64,128,256,512]):
+    def __init__(self, in_channels,  out_channels, features=[64,128,256,512]):
+        super().__init__()
         self.up= nn.ModuleList()
         self.down= nn.ModuleList()
         self.pool= nn.MaxPool2d(kernel_size=2, stride=2)
         #down part
-        for feature in feature:
+        for feature in features:
             self.down.append(DoubleConv(in_channels, feature))
+            in_channels=feature
         
         #up part
         for feature in reversed(features):
             self.up.append(nn.ConvTranspose2d(feature*2, feature, kernel_size=2,stride=2))#this will make the model move up
             self.up.append(DoubleConv(feature*2, feature))#this will do the conv part up 2 conv like this combined
-                           
+        
+        #bottom part
+        self.bottom= DoubleConv(in_features=features[-1], out_features=features[-1]*2)#in feature then out feature 512*2
+        self.final_conv= nn.Conv2d(features[0], out_channels,  kernel_size=1)
+    def forward(self, x):
+        skip=[]
+        #downward pass
+        for index in self.down:
+            x=index(x)
+            skip.append(x)#this is the part where it will skip the connection from the heightest to the lowest connection
+            x=self.pool(x)#160x160-> 80x80
+        x=self.bottom(x)
+        skip= skip[::-1]#reversing the list 
+        for index in range (0,len(self.up), 2):
+            x=self.up[index](x)#up sampling
+            skip_tensor= skip[index//2]#divides and then rounds to nearest whole number
+            concat_skip= torch.cat((skip_tensor, x),dim=1)
+            x= self.up[index+1](concat_skip)
+        return self.final_conv(x)
+
+#test
+x=torch.randn((3,1,160,160))
+model= UNET(in_channels=1, out_channels=1)
+pred= model(x)
+print(pred.shape)
+print(x.shape)
 
